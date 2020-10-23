dc24b6112b76ef716d29a4b9980c442c138cad85
Vyacheslav Basharov <slavbacharov@gmail.com>
2020-10-23 15:32:16 +0200

    Updated from Publii

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,7 @@
 // This is your main script file
-
+<script data-goatcounter="https://existentialist.goatcounter.com/count"
+        async src="//gc.zgo.at/count.js"></script>
+		
 CSS.registerProperty({
 	name: '--defaulAngle', 
 	syntax: '<angle>',
